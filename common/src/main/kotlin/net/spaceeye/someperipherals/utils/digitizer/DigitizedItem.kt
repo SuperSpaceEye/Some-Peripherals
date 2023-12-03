c988401d1cb674c75f60f4d90477b55b74b308f5
spaceeye <superspaceeye@gmail.com>
2023-12-03 20:24:26 +0300

    forge digitizer is working, added peripheral + functionality + borrowed code (hehe)

---
+++ common/src/main/kotlin/net/spaceeye/someperipherals/utils/digitizer/DigitizedItem.kt
@@ -0,0 +1,19 @@
+package net.spaceeye.someperipherals.utils.digitizer
+
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.world.item.ItemStack
+import java.util.*
+
+class DigitizedItem(@JvmField var id: UUID, @JvmField var item: ItemStack) {
+    constructor(tag: CompoundTag):
+            this(
+                tag.getUUID("uuid"),
+                ItemStack.of(tag.getCompound("itemStack"))
+            )
+
+    fun serialize(tag: CompoundTag): CompoundTag {
+        tag.putUUID("uuid", id)
+        tag.put("itemStack", item.save(CompoundTag()))
+        return tag
+    }
+}
