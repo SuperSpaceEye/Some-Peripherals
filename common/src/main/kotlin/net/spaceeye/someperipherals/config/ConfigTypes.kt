e693baded43ffa52c63a97678566c20588f11ed0
spaceeye <superspaceeye@gmail.com>
2024-01-09 15:31:52 +0300

    Changed default comment so that it doesn't spam the log on startup, also why the fuck does max_raycast_time_ms errors like why "Incorrect key SomePeripheralsConfig.RaycastingSettings.max_raycast_time_ms was corrected from 100 to its default, 100." wtf everything else works ok why max_raycast_time_ms specifically???

--- common/src/main/kotlin/net/spaceeye/someperipherals/config/ConfigTypes.kt
+++ common/src/main/kotlin/net/spaceeye/someperipherals/config/ConfigTypes.kt
@@ -2,7 +2,7 @@ package net.spaceeye.someperipherals.config
 
 import kotlin.reflect.KProperty
 
-open class BaseConfigDelegate <T : Any>(var it:T, var range: Pair<T, T>? = null, var description: String="", val do_show:Boolean = true) {
+open class BaseConfigDelegate <T : Any>(var it:T, var range: Pair<T, T>? = null, var description: String="No comment", val do_show:Boolean = true) {
     private lateinit var delegateRegister: DelegateRegisterItem
 
     operator fun getValue(thisRef: Any?, property: KProperty<*>):T {
@@ -20,8 +20,8 @@ open class BaseConfigDelegate <T : Any>(var it:T, var range: Pair<T, T>? = null,
     }
 }
 
-class CInt   (it:Int,     description: String="", range: Pair<Int, Int>?       = null, do_show: Boolean=true): BaseConfigDelegate<Int>    (it, range, description, do_show)
-class CLong  (it:Long,    description: String="", range: Pair<Long, Long>?     = null, do_show: Boolean=true): BaseConfigDelegate<Long>   (it, range, description, do_show)
-class CDouble(it:Double,  description: String="", range: Pair<Double, Double>? = null, do_show: Boolean=true): BaseConfigDelegate<Double> (it, range, description, do_show)
-class CBool  (it:Boolean, description: String="", do_show: Boolean=true): BaseConfigDelegate<Boolean>(it, null, description, do_show)
-class CString(it:String,  description: String="", do_show: Boolean=true): BaseConfigDelegate<String> (it, null, description, do_show)
+class CInt   (it:Int,     description: String="No comment", range: Pair<Int, Int>?       = null, do_show: Boolean=true): BaseConfigDelegate<Int>    (it, range, description, do_show)
+class CLong  (it:Long,    description: String="No comment", range: Pair<Long, Long>?     = null, do_show: Boolean=true): BaseConfigDelegate<Long>   (it, range, description, do_show)
+class CDouble(it:Double,  description: String="No comment", range: Pair<Double, Double>? = null, do_show: Boolean=true): BaseConfigDelegate<Double> (it, range, description, do_show)
+class CBool  (it:Boolean, description: String="No comment", do_show: Boolean=true): BaseConfigDelegate<Boolean>(it, null, description, do_show)
+class CString(it:String,  description: String="No comment", do_show: Boolean=true): BaseConfigDelegate<String> (it, null, description, do_show)
